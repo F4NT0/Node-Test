72d98f3c5ee151110f997ff99e12c0ad34133d67
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-15 13:04:17 -0300

    organize files

--- README.md
+++ README.md
@@ -6,5 +6,3 @@
 2. Instalando o Express no Projeto: `npm install express --save`
 3. Arquivo main definido foi o _index.js_
 4. Criado o arquivo _index.js_
-5. Chamando o Módulo Express em nosso projeto usando função _require_: `var express = require("express");`
-6. Instanciando o Módulo express para podermos usar as suas Funções: `var app = express();`
--- index.js
+++ index.js
@@ -2,14 +2,19 @@
  * TESTE DE FUNÇÕES DO EXPRESS
  * Explicação de Como funciona: https://github.com/F4NT0/estudos-node/tree/master/entendendo-express
  */
+
+// Importando e Instanciando o Express.js
 var express = require("express");
 var app = express();
 
+//
+
 //Módulo Path (para pegar o __dirname)
+var path = require("path");
 
 // Função get
 app.get("/",function(req,res){
-   res.sendFile(__dirname + "/app.component.html");
+   res.sendFile(__dirname + "/app.component.html"); 
 });
 
 app.listen(8080,function(){
