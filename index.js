43d1b9fb673e482584b4b425d142c76c901e59ad
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-15 13:32:09 -0300

    teste finalizado

--- index.js
+++ index.js
@@ -7,15 +7,32 @@
 var express = require("express");
 var app = express();
 
-//
+//Definindo Diretório de leitura
+app.use(express.static("arquivos"));
 
 //Módulo Path (para pegar o __dirname)
 var path = require("path");
 
-// Função get
+// Função get pega um arquivo html e apresenta na rota definida("/" é a Home)
 app.get("/",function(req,res){
-   res.sendFile(__dirname + "/app.component.html"); 
+   res.sendFile(__dirname + "/arquivos/html/index.html"); 
 });
+
+/*
+* OUTRAS ROTAS
+*/
+
+app.get("/sobre",function(req,res){
+    res.sendFile(__dirname + "/arquivos/html/sobre.html");
+});
+
+app.get("/contato",function(req,res){
+    res.sendFile(__dirname + "/arquivos/html/contato.html");
+});
+
+/**
+ * INICIANDO SERVIDOR
+ */
 
 app.listen(8080,function(){
     console.log("Servidor Iniciado!\nAcesse http://localhost:8080/");
